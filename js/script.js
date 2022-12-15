9ad57b3e690292144864530ad6668c38ca9f701f
Antonio-Italiano <antoitalia221@gmail.com>
2022-12-15 08:46:47 +0100

    Add logic

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
+// elementi Dom
 const listaElement = document.getElementById ('lista');
+
+// lista spesa 
+const shoppingList = ['pane', 'salsiccia', 'carbone', 'spirito'];
+
+let i = 0;
+let listPrint = '';
+
+listaElement.innerText = shoppingList[i] ;
+while (i < shoppingList.length) {
+    listPrint += `<div>${shoppingList[i]}</div>`;
+    console.log(shoppingList[i]);    
+    i++;
+}
+
+listaElement.innerHTML = listPrint;
